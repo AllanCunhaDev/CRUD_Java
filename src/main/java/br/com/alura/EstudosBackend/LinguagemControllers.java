667b5d2556c7f73f6c8fd149c33c70e1c02bc534
Allan <allancunha20@hotmail.com>
2023-03-30 23:46:09 -0300

    feat:DbLinguagens

--- src/main/java/br/com/alura/EstudosBackend/LinguagemControllers.java
+++ src/main/java/br/com/alura/EstudosBackend/LinguagemControllers.java
@@ -1,16 +1,25 @@
 package br.com.alura.EstudosBackend;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LinguagemControllers {
 
-    @GetMapping(value = "path")
-    public String getMethodName(@RequestParam String param) {
+    private List<Linguagem> linguagens = List.of(
+            new Linguagem("Java",
+                    "https://raw.githubusercontent.com/AllanCunhaDev/programming-languages-logos/master/src/java/java_256x256.png",
+                    1),
+            new Linguagem("TypeScript",
+                    "https://raw.githubusercontent.com/AllanCunhaDev/programming-languages-logos/master/src/typescript/typescript_256x256.png",
+                    2));
 
-        return "Oi, javinhas!";
+    private LinguagemRepository repositorio;
 
+    @GetMapping("/linguagens")
+    public List<Linguagem> obterLinguagens() {
+        return linguagens;
     }
 }
